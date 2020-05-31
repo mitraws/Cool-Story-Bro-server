d58af8cbe526f089aa4f9f4cacbdaf7500b7dd21
Mitraws <m.ws@outlook.com>
2020-05-31 15:40:35 +0200

    Add detailpage route

--- index.js
+++ index.js
@@ -151,6 +151,9 @@ app.post("/authorized_post_request", authMiddleWare, (req, res) => {
 const authRouter = require("./routers/auth");
 app.use("/", authRouter);
 
+const homepageRouter = require("./routers/homepage")
+app.use("/homepages", homepageRouter);
+
 // Listen for connections on specified port (default is port 4000)
 const { PORT } = require("./config/constants");
 
@@ -158,5 +161,3 @@ app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
 
-const homepageRouter = require("./routers/homepage")
-app.use("/", homepageRouter);
--- routers/homepage.js
+++ routers/homepage.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const Homepage = require("../models").homepage;
+const Story = require("../models").story;
+
 const router = new Router();
 // const { toJWT, toData } = require("../auth/jwt");
 // const authMiddleware = require("../auth/middleware")
@@ -12,6 +14,26 @@ router.get("/", async (req, res, next) => {
     next(e);
   }
 });
+
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    console.log(id);
+    if (isNaN(parseInt(id))) {
+      return res.status(400).send({ message: "Homepage id is not a number" });
+    }
+  
+    const homepage = await Homepage.findByPk(id, {
+      include: [Story],
+      order: [[Story, "createdAt", "DESC"]]
+    });
+
+    if (homepage === null) {
+        return res.status(404).send({ message: "Homepage not found" });
+      }
+    
+      res.status(200).send({ message: "ok", homepage });
+    });
 
 // router.post("/", async (req, res, next) => {
 //   try {
