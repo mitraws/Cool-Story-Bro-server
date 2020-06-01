8fc6f75693ad7743d12323b20400cd79914a9a5c
Mitraws <m.ws@outlook.com>
2020-06-02 00:09:07 +0200

    Add router patch

--- routers/homepage.js
+++ routers/homepage.js
@@ -66,4 +66,19 @@ router.post("/:id/stories", auth, async (req, res) => {
   return res.status(201).send({ message: "Story created", story });
 });
 
+router.patch("/:id", auth, async (req, res) => {
+  const homepage = await Homepage.findByPk(req.params.id);
+  if (!homepage.userId === req.user.id) {
+    return res
+      .status(403)
+      .send({ message: "You are not authorized to update this homepage" });
+  }
+
+  const { title, description, backgroundColor, color } = req.body;
+
+  await homepage.update({ title, description, backgroundColor, color });
+
+  return res.status(200).send({ homepage });
+});
+
 module.exports = router;
